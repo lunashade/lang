3cb54fe196a8633df9bfe8956da1421ea671e6fc
lunashade <word47wonders@gmail.com>
2022-05-07 00:32:01 +0900

    Pass first test

--- main.go
+++ main.go
@@ -1,5 +1,8 @@
 package main
 
+import "fmt"
+
 func main() {
+	fmt.Println("define dso_local i32 @main() #0 { ret i32 0 } ")
 	return
 }
--- test.sh
+++ test.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 # test script for lang
 TARGET=$1  # target executable
-echo "target: ${TARGET}"
-echo ok
-exit 0
+
+function check {
+    want=$1
+    input=$2
+
+    ${TARGET} "$input" | lli
+    got=$?
+    if [[ "$want" != "$got" ]]; then
+        echo "input: {$input}, want: ${want} but got: ${got}";
+        exit 1;
+    fi
+}
+
+
+function main {
+    echo "target: ${TARGET}"
+    check 0 "0"
+    echo ok
+}
+
+main
