68ca3d0047cf199df0fb68665e08a8d498cd8744
lunashade <word47wonders@gmail.com>
2022-05-08 01:27:55 +0900

    Read stdin directly

--- main.go
+++ main.go
@@ -2,22 +2,14 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"os"
-	"strings"
 
 	"github.com/lunashade/lang/internal/token"
 	"github.com/lunashade/lang/internal/token/kind"
 )
 
 func main() {
-	b, err := ioutil.ReadAll(os.Stdin)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err.Error())
-		panic("cannot read stdin")
-	}
-	code := string(b)
-	tokens := token.Lex(strings.NewReader(code))
+	tokens := token.Lex(os.Stdin)
 
 	fmt.Printf("define dso_local i32 @main() #0 {\n")
 	for tok := range tokens {
