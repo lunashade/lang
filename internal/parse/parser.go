2bc161d45edc32826e0054934e34806e976a69e8
lunashade <word47wonders@gmail.com>
2022-05-14 20:31:13 +0900

    Add combinator

--- internal/parse/parser.go
+++ internal/parse/parser.go
@@ -48,16 +48,26 @@ func (p *Parser) Consume(kind kind.Kind, at int) (int, *token.Token) {
 	return at + 1, p.look(at)
 }
 
+func (p *Parser) Skip(kind kind.Kind) NonTerminal {
+	return func(pos int) (int, ast.AST, error) {
+		nx, t := p.Consume(kind, pos)
+		if t == nil {
+			return pos, nil, errors.New("invalid token")
+		}
+		return nx, nil, nil
+	}
+}
+
 func Parse(ch chan token.Token) ast.AST {
 	p := &Parser{
 		ch:     ch,
 		tokens: make([]token.Token, 0),
 	}
-	ast, err := p.Root(0)
+	node, err := p.Root(0)
 	if err != nil {
 		panic(err)
 	}
-	return ast
+	return node
 }
 
 // PEG
@@ -70,131 +80,108 @@ func Parse(ch chan token.Token) ast.AST {
 // [Div] <- Term "/" Prod
 // Term <- Integer
 func (p *Parser) Root(pos int) (ast.AST, error) {
-	// TODO: will be like below
-	_, ast, err := p.Sum(0)
+	_, node, err := p.Sum(0)
 	if !p.ateof {
 		panic("not at eof")
 	}
 	if err != nil {
 		panic(err)
 	}
-	return ast, nil
+	return node, nil
 }
 
 type NonTerminal func(int) (int, ast.AST, error)
 
-func (p *Parser) Sum(pos int) (int, ast.AST, error) {
-	var nx int
-	var ast ast.AST
-	var err error
+func (p *Parser) Select(cands ...NonTerminal) NonTerminal {
+	return func(pos int) (int, ast.AST, error) {
+		var nx int
+		var node ast.AST
+		var err error
+		for _, cand := range cands {
+			nx, node, err = cand(pos)
+			if err == nil {
+				return nx, node, nil
+			}
+		}
+		// TODO: wraps error
+		return pos, nil, err
+	}
+}
 
-	nx, ast, err = p.Add(pos)
-	if err == nil {
-		return nx, ast, nil
+type Merger func([]ast.AST) ast.AST
+
+func (p *Parser) Concat(m Merger, cands ...NonTerminal) NonTerminal {
+	return func(pos int) (int, ast.AST, error) {
+		var nx int = pos
+		var node ast.AST
+		var err error
+
+		nodes := make([]ast.AST, 0)
+		for _, cand := range cands {
+			nx, node, err = cand(nx)
+			if err != nil {
+				return pos, nil, err
+			}
+			nodes = append(nodes, node)
+		}
+		return nx, m(nodes), nil
 	}
-	nx, ast, err = p.Sub(pos)
-	if err == nil {
-		return nx, ast, nil
-	}
-	nx, ast, err = p.Prod(pos)
-	if err == nil {
-		return nx, ast, nil
-	}
-	return pos, nil, err
+
+}
+
+func (p *Parser) Sum(pos int) (int, ast.AST, error) {
+	return p.Select(p.Add, p.Sub, p.Prod)(pos)
 }
 func (p *Parser) Prod(pos int) (int, ast.AST, error) {
-	var nx int
-	var ast ast.AST
-	var err error
-
-	nx, ast, err = p.Mul(pos)
-	if err == nil {
-		return nx, ast, nil
-	}
-	nx, ast, err = p.Div(pos)
-	if err == nil {
-		return nx, ast, nil
-	}
-	nx, ast, err = p.Term(pos)
-	if err == nil {
-		return nx, ast, nil
-	}
-	return pos, nil, err
+	return p.Select(p.Mul, p.Div, p.Term)(pos)
 }
 
 func (p *Parser) Add(pos int) (int, ast.AST, error) {
-	// read LHS
-	nx, lhs, err := p.Prod(pos)
-	if err != nil {
-		return pos, nil, err
-	}
-	// skip "+"
-	nx, t := p.Consume(kind.Plus, nx)
-	if t == nil {
-		return pos, nil, errors.New("invalid token")
-	}
-	// read RHS
-	nx, rhs, err := p.Sum(nx)
-
-	// return
-	return nx, &ast.Add{LHS: lhs, RHS: rhs}, nil
+	return p.Concat(
+		func(nodes []ast.AST) ast.AST {
+			return &ast.Add{LHS: nodes[0], RHS: nodes[2]}
+		},
+		p.Prod,
+		p.Skip(kind.Plus),
+		p.Sum,
+	)(pos)
 }
-func (p *Parser) Sub(pos int) (int, ast.AST, error) {
-	// read LHS
-	nx, lhs, err := p.Prod(pos)
-	if err != nil {
-		return pos, nil, err
-	}
-	// skip "+"
-	nx, t := p.Consume(kind.Minus, nx)
-	if t == nil {
-		return pos, nil, errors.New("invalid token")
-	}
-	// read RHS
-	nx, rhs, err := p.Sum(nx)
 
-	// return
-	return nx, &ast.Sub{LHS: lhs, RHS: rhs}, nil
+func (p *Parser) Sub(pos int) (int, ast.AST, error) {
+	return p.Concat(
+		func(nodes []ast.AST) ast.AST {
+			return &ast.Sub{LHS: nodes[0], RHS: nodes[2]}
+		},
+		p.Prod,
+		p.Skip(kind.Minus),
+		p.Sum,
+	)(pos)
 }
 
 func (p *Parser) Mul(pos int) (int, ast.AST, error) {
-	// read LHS
-	nx, lhs, err := p.Term(pos)
-	if err != nil {
-		return pos, nil, err
-	}
-	// skip "+"
-	nx, t := p.Consume(kind.Multiply, nx)
-	if t == nil {
-		return pos, nil, errors.New("invalid token")
-	}
-	// read RHS
-	nx, rhs, err := p.Prod(nx)
-
-	// return
-	return nx, &ast.Mul{LHS: lhs, RHS: rhs}, nil
+	return p.Concat(
+		func(nodes []ast.AST) ast.AST {
+			return &ast.Mul{LHS: nodes[0], RHS: nodes[2]}
+		},
+		p.Term,
+		p.Skip(kind.Multiply),
+		p.Prod,
+	)(pos)
 }
 
 func (p *Parser) Div(pos int) (int, ast.AST, error) {
-	// read LHS
-	nx, lhs, err := p.Term(pos)
-	if err != nil {
-		return pos, nil, err
-	}
-	// skip "+"
-	nx, t := p.Consume(kind.Divide, nx)
-	if t == nil {
-		return pos, nil, errors.New("invalid token")
-	}
-	// read RHS
-	nx, rhs, err := p.Prod(nx)
-
-	// return
-	return nx, &ast.Div{LHS: lhs, RHS: rhs}, nil
+	return p.Concat(
+		func(nodes []ast.AST) ast.AST {
+			return &ast.Div{LHS: nodes[0], RHS: nodes[2]}
+		},
+		p.Term,
+		p.Skip(kind.Divide),
+		p.Prod,
+	)(pos)
 }
 
 func (p *Parser) Term(pos int) (int, ast.AST, error) {
-	return p.Integer(pos)
+	return p.Select(p.Integer)(pos)
 }
 func (p *Parser) Integer(pos int) (int, ast.AST, error) {
 	nx, t := p.Consume(kind.Integer, pos)
--- internal/parse/parser_test.go
+++ internal/parse/parser_test.go
@@ -23,6 +23,37 @@ func TestParse(t *testing.T) {
 				RHS: &ast.Int{Value: 1},
 			},
 		},
+		{
+			"1-1", "1-1",
+			&ast.Sub{
+				LHS: &ast.Int{Value: 1},
+				RHS: &ast.Int{Value: 1},
+			},
+		},
+		{
+			"1*1", "1*1",
+			&ast.Mul{
+				LHS: &ast.Int{Value: 1},
+				RHS: &ast.Int{Value: 1},
+			},
+		},
+		{
+			"1/1", "1/1",
+			&ast.Div{
+				LHS: &ast.Int{Value: 1},
+				RHS: &ast.Int{Value: 1},
+			},
+		},
+		{
+			"1+1*1", "1+1*1",
+			&ast.Add{
+				LHS: &ast.Int{Value: 1},
+				RHS: &ast.Mul{
+					LHS: &ast.Int{Value: 1},
+					RHS: &ast.Int{Value: 1},
+				},
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
