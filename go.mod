06c9e0f73b7ee741fac4822e6af1f5dac8c0c0dd
lunashade <word47wonders@gmail.com>
2022-05-23 00:35:47 +0900

    Use llir/llvm instead of directly writing strings

--- go.mod
+++ go.mod
@@ -6,5 +6,14 @@ require gotest.tools v2.2.0+incompatible
 
 require (
 	github.com/google/go-cmp v0.5.8 // indirect
+	github.com/kr/pretty v0.2.0 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/llir/ll v0.0.0-20210719001141-246f2b6b1fa9 // indirect
+	github.com/llir/llvm v0.3.4 // indirect
+	github.com/mewmew/float v0.0.0-20201204173432-505706aa38fa // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/mod v0.4.2 // indirect
+	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
+	golang.org/x/tools v0.1.4 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 )
--- internal/gen/gen.go
+++ internal/gen/gen.go
@@ -2,106 +2,87 @@ package gen
 
 import (
 	"errors"
-	"fmt"
 	"io"
 
+	"github.com/llir/llvm/ir"
+	"github.com/llir/llvm/ir/constant"
+	"github.com/llir/llvm/ir/types"
+	"github.com/llir/llvm/ir/value"
 	"github.com/lunashade/lang/internal/ast"
 )
 
-type Reg int
-
-const InvalidReg Reg = -1
-
 type Generator struct {
-	w           io.Writer
-	reg         Reg
-	indentDepth int
-}
-
-func (g *Generator) nextReg() Reg {
-	g.reg++
-	return g.reg
+	m        *ir.Module
+	curFunc  *ir.Func
+	curBlock *ir.Block
 }
 
 func Run(w io.Writer, tree ast.AST) error {
-	g := &Generator{w: w}
-	root, ok := tree.(*ast.Root)
-	if !ok {
-		return errors.New("must be root")
+	g := &Generator{
+		m: ir.NewModule(),
 	}
-	return g.Root(root)
-}
-
-func (g *Generator) Root(root *ast.Root) error {
-	var reg Reg
-	var err error
-	g.emitf("define dso_local i32 @main() #0 {")
-	g.indentDepth++
-	for _, expr := range root.Nodes {
-		if reg, err = g.Expr(expr); err != nil {
-			return err
-		}
+	if err := g.walk(tree); err != nil {
+		return err
 	}
-	g.emitf("ret i32 %%%d", reg)
-	g.indentDepth--
-	g.emitf("}")
+	g.m.WriteTo(w)
 	return nil
 }
 
-func (g *Generator) Expr(expr ast.AST) (Reg, error) {
-	switch nd := expr.(type) {
-	case *ast.BinOp:
-		return g.BinOp(nd)
-	case *ast.Int:
-		return g.Int(nd)
-	default:
-		return InvalidReg, errors.New("unknown expression")
+func (g *Generator) walk(node ast.AST) error {
+	switch nd := node.(type) {
+	case *ast.Root:
+		var val value.Value
+		var err error
+		g.curFunc = g.m.NewFunc("main", types.I32)
+		g.curBlock = g.curFunc.NewBlock("")
+		for _, expr := range nd.Nodes {
+			val, err = g.expr(expr)
+			if err != nil {
+				return err
+			}
+		}
+		g.curBlock.NewRet(val)
 	}
-
+	return nil
 }
 
-func (g *Generator) BinOp(op *ast.BinOp) (Reg, error) {
-	lhs, err := g.Expr(op.LHS)
-	if err != nil {
-		return InvalidReg, err
+func (g *Generator) expr(node ast.AST) (value.Value, error) {
+	switch nd := node.(type) {
+	case *ast.Int:
+		val := constant.NewInt(types.I32, int64(nd.Value))
+		alloc := g.curBlock.NewAlloca(types.I32)
+		g.curBlock.NewStore(val, alloc)
+		return g.curBlock.NewLoad(types.I32, alloc), nil
+	case *ast.BinOp:
+		return g.binOp(nd)
+	default:
+		return nil, errors.New("unknown expr")
 	}
-	rhs, err := g.Expr(op.RHS)
+}
+
+func (g *Generator) binOp(node *ast.BinOp) (value.Value, error) {
+	lhs, err := g.expr(node.LHS)
 	if err != nil {
-		return InvalidReg, err
+		return nil, err
+	}
+	rhs, err := g.expr(node.RHS)
+	if err != nil {
+		return nil, err
 	}
 
-	res := g.nextReg()
-	switch op.Type {
+	switch node.Type {
 	case ast.Add:
-		g.emitf("%%%d = add i32 %%%d, %%%d", res, lhs, rhs)
+		res := g.curBlock.NewAdd(lhs, rhs)
 		return res, nil
 	case ast.Sub:
-		g.emitf("%%%d = sub i32 %%%d, %%%d", res, lhs, rhs)
+		res := g.curBlock.NewSub(lhs, rhs)
 		return res, nil
 	case ast.Mul:
-		g.emitf("%%%d = mul i32 %%%d, %%%d", res, lhs, rhs)
+		res := g.curBlock.NewMul(lhs, rhs)
 		return res, nil
 	case ast.Div:
-		g.emitf("%%%d = sdiv i32 %%%d, %%%d", res, lhs, rhs)
+		res := g.curBlock.NewSDiv(lhs, rhs)
 		return res, nil
 	}
-	return InvalidReg, errors.New("unknown operator")
-}
-
-func (g *Generator) Int(i *ast.Int) (Reg, error) {
-	alloc := g.nextReg()
-	result := g.nextReg()
-	g.emitf("%%%d = alloca i32, align 4", alloc)
-	g.emitf("store i32 %d, i32* %%%d", i.Value, alloc)
-	g.emitf("%%%d = load i32, i32* %%%d, align 4", result, alloc)
-	return result, nil
-}
-
-func (g *Generator) emitf(format string, a ...interface{}) error {
-	for i := 0; i < g.indentDepth; i++ {
-		fmt.Fprintf(g.w, "\t")
-	}
-	_, err := fmt.Fprintf(g.w, format, a...)
-	fmt.Fprintf(g.w, "\n")
-	return err
+	return nil, errors.New("unknown operator")
 }
